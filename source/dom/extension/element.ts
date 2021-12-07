b453b479af01197673aacc369b33fef63bc5b14e
Ziphil <yfe23457@nifty.com>
2021-12-07 20:01:24 +0900

    メソッドを追加

--- source/dom/extension/element.ts
+++ source/dom/extension/element.ts
@@ -3,6 +3,18 @@
 import "../constructor";
 
 
+function getChildElements(this: Element, tagName: string): Array<Element> {
+  let nodes = this.childNodes;
+  let elements = [];
+  for (let i = 0 ; i < nodes.length ; i ++) {
+    let node = nodes.item(i);
+    if (node.isElement() && node.tagName === tagName) {
+      elements.push(node);
+    }
+  }
+  return elements;
+}
+
 function getDescendantTexts(this: Element): Array<Text> {
   let texts = [];
   for (let child of Array.from(this.childNodes)) {
@@ -21,6 +33,7 @@ Element.prototype.getDescendantTexts = getDescendantTexts;
 declare global {
 
   interface Element {
+    getChildElements: typeof getChildElements;
     getDescendantTexts: typeof getDescendantTexts;
   }
 
