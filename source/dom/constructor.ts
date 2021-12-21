bea650aed3c760a204d0572e58c238b5b6619e93
Ziphil <yfe23457@nifty.com>
2021-12-22 01:31:18 +0900

    動かなくなっていたのを修正

--- source/dom/constructor.ts
+++ source/dom/constructor.ts
@@ -13,7 +13,7 @@ let comment = document.createComment("dummy");
 
 let global = globalThis as any;
 
-global.Node ??= require("xmldom/lib/dom").Node;
+global.Node ??= require("@xmldom/xmldom/lib/dom").Node;
 global.Element ??= element.constructor as any;
 global.Text ??= text.constructor as any;
 global.Comment ??= comment.constructor as any;
